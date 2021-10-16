5418eba397077463c9281383c74db9a9be751c90
jhonatanMD <jhonatanmd1999@gmail.com>
2021-10-15 23:18:07 -0500

    add

--- src/main/java/com/main/ProjectInit.java
+++ src/main/java/com/main/ProjectInit.java
@@ -12,7 +12,7 @@ public class ProjectInit {
         //CREACIÓN DE LISTA CON ARRAYS.ASLIST -> INMUTABLES -> NO SE PUEDEN EDITAR
         List<String> palabras = Arrays.asList("1","2","3","4");
 
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> numeros = new ArrayList<>(Arrays.asList(6,1,4,37,6,8,2,0,9,5,90,21,10,21,90,7));
 
         //palabras.forEach(System.out::println);
 
@@ -48,10 +48,64 @@ public class ProjectInit {
 
         //CONTAINS SABER SI EXISTE EL OBJETO
         //System.out.println(palabras3.contains("ZAMIR"));
-        palabras3.forEach(System.out::println);
 
-        System.out.println("TAMAÑO DE LISTA : "+palabras3.size());
 
+
+
+        //palabras3.forEach(System.out::println);
+
+        //System.out.println("TAMAÑO DE LISTA : "+palabras3.size());
+
+
+        //numeros.forEach(n -> System.out.println(n));
+
+        /*for(Integer n: numeros) {
+            System.out.println(n);
+        }*/
+
+        //numeros.forEach(System.out::println);
+
+        /*numeros.forEach(n -> {
+
+            if(n == 5){
+                System.out.println("N -> 5");
+            }
+            System.out.println(n);
+        });*/
+
+
+        //SORT EN LIST ->  ORDENAR ITEMS
+        //numeros.sort((a,b) -> a.compareTo(b));
+        //numeros.forEach(System.out::println);
+
+        //SORT EN STREAM API -> ORDERNAR ITEMS
+        //numeros.stream().sorted().forEach(System.out::println);
+        //numeros.stream().sorted((a,b) -> b.compareTo(a)).forEach(System.out::println);
+
+
+        //DISTINCT EN STREAM API -> TRAER DATOS UNICOS
+        //numeros.stream().distinct().forEach(System.out::println);
+
+
+        /*numeros.stream()
+                .filter(n -> n >= 21)
+                .sorted()
+                .distinct()
+                .forEach(System.out::println);
+
+         */
+
+        //MAP -> CONVERSIONES -> DEVUELVE EL MISMO OBJETO
+        //numeros.stream().map(n -> n + " JMD ").forEach(System.out::println);
+
+        //FLATMAP -> CONVERSION DE DATOS PERO DEVUELVE UNA LISTA -> STREAM
+        /*numeros.stream().flatMap(n -> {
+            return palabras.stream();
+        }).forEach(System.out::println);
+         */
+
+        //REDUCIR LA LISTA
+        //numeros.stream().reduce((a,b) -> a+b).stream().forEach(System.out::println);
     }
 
 }
