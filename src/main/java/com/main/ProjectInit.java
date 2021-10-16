1bf822b13d99541c7522f6c2913fe333c0f538d8
jhonatanMD <jhonatanmd1999@gmail.com>
2021-10-15 22:41:02 -0500

    add

--- src/main/java/com/main/ProjectInit.java
+++ src/main/java/com/main/ProjectInit.java
@@ -10,7 +10,7 @@ public class ProjectInit {
 
         //List  -> Son colecciones
         //CREACIÓN DE LISTA CON ARRAYS.ASLIST -> INMUTABLES -> NO SE PUEDEN EDITAR
-        List<String> palabras = Arrays.asList("HOLA","CHAU","BYE","ADIOS");
+        List<String> palabras = Arrays.asList("1","2","3","4");
 
         List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
@@ -23,14 +23,31 @@ public class ProjectInit {
         List<String> palabras3 = new ArrayList<>(Arrays.asList("HOLA","CHAU","BYE","ADIOS"));
 
         //ADD ->  AGREGAR NUEVOS ITEMS A LA LISTA
-        palabras3.add("ESTA CLASE JAVA");
+        //palabras3.add("ESTA CLASE JAVA");
 
         //REMOVE -> RETIRAR OBJETO DE LA LISTA
-        palabras3.remove(0);
-        palabras3.remove("BYE");
-        palabras3.removeIf(item -> item.equals("ADIOS"));
+        //palabras3.remove(0);
+        //palabras3.remove("BYE");
+        //palabras3.removeIf(item -> item.equals("ADIOS"));
 
+
+
+        //"JAVA","HOLA","CHAU","BYE","ADIOS"
+        //palabras3.add(3,"JAVA");
+
+        //ADDALL -> AGREGAR ITEMS MEDIANTE UNA LISTA
+        //palabras3.addAll(palabras);
+
+        //SET -> REEMPLAZAR EL OBJETO MEDIANTE SU INDICE
+        //palabras3.set(0,"GOOD BYE");
+
+        // SIZE -> SABER LA CANTIDAD DE ITEMS QUE TIENE MI LISTA
+
+        //INDEX OF DEVUEL EL INDICE DEL OBJETO
+        System.out.println(palabras3.indexOf("CHAU"));
         palabras3.forEach(System.out::println);
+
+        System.out.println("TAMAÑO DE LISTA : "+palabras3.size());
 
     }
 
